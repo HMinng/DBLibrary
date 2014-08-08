7633b0b4f70b35d72cf5a6da795645a23ba5b0b8
HMinng <jackhm18@gmail.com>
2014-08-08 10:18:27 +0800

    change yml file autoload

--- src/AtsConfig/AtsConfig.php
+++ src/AtsConfig/AtsConfig.php
@@ -5,11 +5,9 @@ use Symfony\Component\Yaml\Yaml;
 
 class AtsConfig
 {
-    private static $file = '/Users/HMinng/HMinng/workspaces/Video/application/library/config/AtsConfig.yml';
-    
 	public static function configures()
 	{
-	    $configures = Yaml::parse(self::$file);
+	    $configures = Yaml::parse(APPLICATION_PATH . '/../application/library/config/AtsConfig.yml');
 	    
 	    return $configures['databases'];
 	}
