2c8f1b786a0db2529f624b32c83a242ce40e1108
HMinng <jackhm18@gmail.com>
2014-12-01 11:33:17 +0800

    fied ymal

--- src/AtsConfig/AtsConfig.php
+++ src/AtsConfig/AtsConfig.php
@@ -7,7 +7,7 @@ class AtsConfig
 {
 	public static function configures()
 	{
-	    $configures = Yaml::parse(APPLICATION_PATH . '/../conf/custom/Databases.yml');
+	    $configures = Yaml::parse(file_get_contents(APPLICATION_PATH . '/../conf/custom/Databases.yml'));
 
         $env = defined('APPLICATION_ENV') ? APPLICATION_ENV: 'product';
 
