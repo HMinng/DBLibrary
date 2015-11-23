d66b19581869ab427e6e4ba41913fe3c88f3d227
HMinng <jackhm18@gmail.com>
2015-11-23 16:26:10 +0800

    修改配置

--- src/AtsConfig/AtsConfig.php
+++ src/AtsConfig/AtsConfig.php
@@ -1,13 +1,11 @@
 <?php
 namespace HMinng\DBLibrary\AtsConfig;
 
-use Symfony\Component\Yaml\Yaml;
-
 class AtsConfig
 {
 	public static function configures()
 	{
-	    $configures = Yaml::parse(file_get_contents(APPLICATION_PATH . '/../conf/custom/Databases.yml'));
+	    $configures = yaml_parse_file(APPLICATION_PATH . '/../conf/custom/data.yml');
 
         $env = defined('APPLICATION_ENV') ? APPLICATION_ENV: 'product';
 
